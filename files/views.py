eb13c53a50cf421a24b3dab402b6d7083d0112f7
Volodymyr Yakymenko <vovkayakimenko2564@gmail.com>
2022-11-27 21:43:07 +0200

    Validation done

--- files/views.py
+++ files/views.py
@@ -4,7 +4,7 @@ from files.imports import *
 CATEGORIES = []
 USERS = []
 RECORDS = []
-GET_RECORDS_RESULTS = {}
+RECORDS_RESULTS = {}
 
 
 def create_user_data():
@@ -42,23 +42,14 @@ def get_array_string(array):
 
 
 def write_to_file(array, directory="logs", filename='', default_key="JSON"):
-    def get_list_name():
-        return [i for i, j in globals().items() if j == array][0].lower()
-
-    # default file extension - .json
-    if filename == '': filename = get_list_name() + ".json"
-
     path = os.path.join(os.getcwd(), directory)
 
     try:
         if not os.path.exists(path): os.mkdir(path)
     except FileNotFoundError: print('Cannot create file :(')
     finally:
-        temp_elements = array if array != GET_RECORDS_RESULTS \
-            else get_array_string(GET_RECORDS_RESULTS)
-
         path = os.path.join(path, filename)
-        result = json.loads(get_array_string(temp_elements))
+        result = json.loads(get_array_string(array))
 
         with open(path, 'w') as file:
             json.dump({default_key: result}, file, indent=4)
@@ -72,7 +63,7 @@ def index():
 @app.route("/categories")
 def get_categories():
     default_key = "Categories"
-    write_to_file(CATEGORIES, default_key=default_key)
+    write_to_file(CATEGORIES, default_key=default_key, filename='categories.json')
 
     return jsonify({default_key: CATEGORIES})
 
@@ -96,7 +87,7 @@ def create_category():
 @app.route("/users")
 def get_users():
     default_key = "Users"
-    write_to_file(USERS, default_key=default_key)
+    write_to_file(USERS, default_key=default_key, filename='users.json')
 
     return jsonify({default_key: USERS})
 
@@ -120,7 +111,7 @@ def create_user():
 @app.route("/records")
 def get_records():
     default_key = "Records"
-    write_to_file(RECORDS, default_key=default_key)
+    write_to_file(RECORDS, default_key=default_key, filename='records.json')
 
     return jsonify({default_key: RECORDS})
 
@@ -149,7 +140,7 @@ def create_record():
     else:
         for i in keys:
             temp_dict[i] = temp_array[keys.index(i)]
-            if keys.index(i) == 1: temp_dict["Date"] = datetime.now()
+            if keys.index(i) == 1: temp_dict["Date"] = datetime.now().strftime("%Y-%m-%d, %H:%M:%S")
 
         RECORDS.append(temp_dict)
 
@@ -158,38 +149,37 @@ def create_record():
 
 @app.route("/user-records")
 def show_user_records():
-    return jsonify({"Users Records": GET_RECORDS_RESULTS})
+    return jsonify({"Users Records": RECORDS_RESULTS})
 
 
 @app.route("/user-record")
 def get_user_records():
-    request_user_id = request.get_json()
-    user_number = request_user_id.get("User")
-    id_category = request_user_id.get("Category")
-    category_name = ''
+    request_user_request = request.get_json()
+    temp_username = ''
+    temp_category = request_user_request.get("Category")
 
-    if not exists(USERS, user_number):
-        return jsonify("This user doesn't exist.")
-    else: user_id = USERS[user_number - 1].get("ID")
+    if validate(request_user_request, "Username"):
+        temp_username = request_user_request.get("Username")
+    else: abort(404, message="Bad request: Username not found!")
 
-    if id_category is not None:
-        if not exists(CATEGORIES, id_category):
-            return jsonify("This category doesn't exist.")
-        else: category_name = CATEGORIES[id_category - 1].get("Name")
-    else: pass
+    if not exists(USERS, temp_username, default_key="Username"):
+        abort(404, message="This user doesn't exist.")
+
+    if temp_category is not None and not exists(CATEGORIES, temp_category):
+        abort(404, message="This category doesn't exist.")
 
     user_records_temp = []
-    key = f'User %s' % user_id + \
-          (f', Category %s' % category_name if category_name != '' else '')
+    key = f'User %s' % temp_username + \
+          (f', Category %s' % CATEGORIES[temp_category - 1].get("Name") if temp_category is not None else '')
 
     for i in RECORDS:
-        if i.get("User ID") == user_id and \
-                (category_name == '' or i.get("Category Name") == category_name):
+        if i.get("Username") == temp_username and \
+                (temp_category is None or i.get("Category") == CATEGORIES[temp_category - 1].get("Name")):
             user_records_temp.append(i)
 
-    GET_RECORDS_RESULTS[key] = user_records_temp
+    RECORDS_RESULTS[key] = user_records_temp
 
-    write_to_file(GET_RECORDS_RESULTS, default_key='Users Records', filename='user-records.json')
+    write_to_file(RECORDS_RESULTS, default_key='Users Records', filename='user-records.json')
 
     return jsonify({key: user_records_temp})
 
